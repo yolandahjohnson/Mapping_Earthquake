58a292969654e5200abf8390c725eaacea26439c
yolandahjohnson <100816778+yolandahjohnson@users.noreply.github.com>
2022-06-04 08:11:43 -0400

    Create logic.js

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,70 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup();
+    }
+}).addTo(map);
+
+// Coordinates for each point to be used in the polyline
+let line = [
+    [33.9416,-118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// let line = [
+    
+//    [37.615983, -122.392477],
+//   [30.1974292, -97.6663058],
+//    [43.6777176, -79.6248196],
+//    [40.641766, -73.780968]
+    
+// ];
+
+
+// Create a polyline using the line coordinates and make the line red, then yellow.
+//  L.polyline(line, {
+//    color: "red"
+//    color: "yellow"
+//  }).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+//We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
